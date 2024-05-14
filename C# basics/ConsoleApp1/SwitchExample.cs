67a114bb37cedde5a5df979f8538977875d8984f
Felipe <afgomez0622@gmail.com>
2024-05-13 20:16:47 -0500

    This code uas used to put into practice, parse, ToString and GetType and casting

---
+++ C# basics/ConsoleApp1/SwitchExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool eligibility = bool.Parse("true");
+            int intfromstr = int.Parse("100");
+            double doublefromstr = double.Parse("100.20");
+
+            string strVal = doublefromstr.ToString();
+            Console.WriteLine($"Data type : {strVal.GetType()}");
+            double dblNum = 3423.20;
+            Console.WriteLine($"Integer: {(int)dblNum}");
+
+
+
+        }
+    }
+}
