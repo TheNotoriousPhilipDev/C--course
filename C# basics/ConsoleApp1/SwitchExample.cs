459f8aa0d76e636b493324fc8d432c635bcc40bb
Felipe <afgomez0622@gmail.com>
2024-06-03 09:24:27 -0500

    branch 05

--- C# basics/ConsoleApp1/SwitchExample.cs
+++ C# basics/ConsoleApp1/SwitchExample.cs
@@ -4,55 +4,24 @@ namespace ConsoleApp1
 {
     public class Program
     {
-
-        // ------------FUNCTIONS------------
-        static void PrinArray(int[] intArray, string mess)
-        {
-            foreach (int i in intArray)
-            {
-                Console.WriteLine("{0} : {1}", mess, i);
-            }
-        }
-
-        //------------END OF FUNCTIONS------------
         static void Main(string[] args)
         {
-            int[] numbers = new int[3];
-            numbers[0] = 1;
-            numbers[1] = 2;
-            numbers[2] = 3;
+            Random random = new Random();
+            int secretNumber = random.Next(1, 11); // minimun value and maximum value
+            int numberGuessed = 0;
+            Console.WriteLine("random num: ", secretNumber);
 
-            Console.WriteLine("Vamos a imprimir algún numero del array numbers: {0}, {1}", numbers[0], numbers[1]);
-
-            string[] customers = { "bob", "sally", "sue", };
-            var employees = new[] { "mike", "paul" };
-            object[] randomArray = { "paul", 45, 1.234 };
-            Console.WriteLine("randomArray 0: {0}", randomArray[0].GetType());
-            Console.WriteLine("Array size : {0}", randomArray.Length);
-            for (int i = 0; i < randomArray.Length; i++)
+            do
             {
-                Console.WriteLine(randomArray[i]);
-            }
-            Console.WriteLine("--------------------------");
+                Console.WriteLine("Enter a number between 1 and 10: ");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
 
-            string[,] custNames = new string[2, 2] { { "Bob", "Carlos" }, { "Sally", "Smith" } };
-            Console.WriteLine("MD Value: {0}",
-                custNames.GetValue(1, 1));
+            } while (numberGuessed != secretNumber);
 
-            int[] randNums = { 1, 2, 3, 4, };
-            PrinArray(randNums, "foreach");
+            Console.WriteLine($"You guessed it, it was: {secretNumber}");
 
-            Console.WriteLine("--------------------------------------");
-
-            Array.Sort(randNums);
-            Array.Reverse(randNums);
-            Array.IndexOf(randNums, 0);
-            randNums.SetValue(0, 0);
-            int[] srcArray = { 1, 2, };
 
         }
-
-
 
     }
 }
