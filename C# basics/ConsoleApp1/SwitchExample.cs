1046cb9f5eb854652366219d3790d790747541ff
Felipe <afgomez0622@gmail.com>
2024-06-03 19:46:24 -0500

    done

--- C# basics/ConsoleApp1/SwitchExample.cs
+++ C# basics/ConsoleApp1/SwitchExample.cs
@@ -6,22 +6,22 @@ namespace ConsoleApp1
 {
     public class Program
     {
+
+        static void PaintCar(CarColor c)
+        {
+            Console.WriteLine("The car was painted {} with the code {1}", c, (int)c);
+        }
+        enum CarColor : byte
+        {
+            orange = 0,
+            green,
+            red,
+            yellow,
+        }
         static void Main(string[] args)
         {
-            DateTime awesomeDate = new DateTime(2020, 12, 30);
-            Console.WriteLine(awesomeDate.ToString());
-            awesomeDate = awesomeDate.AddDays(5);
-            Console.WriteLine(awesomeDate);
-            TimeSpan lunchTime = new TimeSpan(12, 30, 0);
-
-            DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 15);
-            DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
-
-            TimeSpan interval = date2 - date1;
-            Console.WriteLine("{0} - {1} = {2}", date2, date1, interval);
-            lunchTime = lunchTime.Subtract(new TimeSpan(0, 15, 0));
-            Console.WriteLine("New Time: {0}", lunchTime.ToString());
-
+            CarColor car1 = CarColor.orange;
+            PaintCar(car1);
 
         }
 
